ffa2cdbb93268264c60f89d10888b2e9c1bdfde5
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-04-01 18:48:28 +0200

    Changed the S3 in the deployment to a lambda and api gateway.

--- lib/ble_speed_test_server-stack.ts
+++ lib/ble_speed_test_server-stack.ts
@@ -1,14 +1,19 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class BleSpeedTestServerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     })
   }
 }
